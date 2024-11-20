375c1536e40d0bb84d6d1f868e1972c7748c5202
tmdqja75 <tmdqja75@gmail.com>
2024-11-21 02:05:30 +0900

    bentoml LLM 서빙코드

--- app/llm_server/service.py
+++ app/llm_server/service.py
@@ -1,6 +1,10 @@
+import os
 import bentoml
-
 import torch
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Define the BentoML Service
 @bentoml.service
@@ -8,8 +12,23 @@ class LlmService:
     def __init__(self):
         # Load the tokenizer and model
         from transformers import AutoTokenizer, AutoModelForCausalLM
-        self.tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
-        self.model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+        self.tokenizer = AutoTokenizer.from_pretrained(os.getenv("LLM_MODEL"))
+        self.model = AutoModelForCausalLM.from_pretrained(os.getenv("LLM_MODEL"))
         if torch.cuda.is_available():
             self.model.to("cuda")
 
+    @bentoml.api()
+    def generate(self, prompt: str) -> str:
+        # Tokenize the input prompt
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        if torch.cuda.is_available():
+            inputs = {k: v.to("cuda") for k, v in inputs.items()}
+
+        # Generate text
+        with torch.no_grad():
+            outputs = self.model.generate(**inputs, max_new_tokens=os.getenv('MAX_TOKENS'))
+
+        # Decode the generated text
+        generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return generated_text
