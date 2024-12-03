4b323e3ecc82c648f3670704629bea945137f766
tmdqja75 <tmdqja75@gmail.com>
2024-12-03 23:30:42 +0900

    [fix] custom vectorstore 패키지 import 문제 해결

--- app/langgraph/tools.py
+++ app/langgraph/tools.py
@@ -1,10 +1,22 @@
+import getpass
+import os
+import sys
+
 from langchain_core.tools import tool
 from langchain_community.tools import DuckDuckGoSearchRun
+
+# custom 패키지 import 위해 sys.path에 Parent directory 추가
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
 from vectorstore.opensearch_hybrid import OpenSearchHybridSearch
 
 # tool에 필요한 class 초기화
 duckduckgo_search = DuckDuckGoSearchRun()
-opensearch = OpenSearchHybridSearch()
+
+opensearch_password = getpass.getpass("Enter your Opensearch password: ")
+opensearch = OpenSearchHybridSearch(user="admin", pw=opensearch_password)
+
 
 @tool(parse_docstring=True)
 def web_search(query: str):
@@ -19,8 +31,9 @@ def web_search(query: str):
     res = duckduckgo_search.invoke({"query": query})
     return [res]
 
+
 @tool(parse_docstring=True)
-def vectorstore_search(query:str, search_type:str="hybrid"):
+def vectorstore_search(query: str, search_type: str = "hybrid"):
     """search OpenSearch vector databsee for documents that are relevant to a given query
 
     Args:
@@ -28,7 +41,7 @@ def vectorstore_search(query:str, search_type:str="hybrid"):
         search_type (str, optional): one of three similarity search method: hybrid, BM25, and Cosine similarity search Defaults to "hybrid".
 
     Returns:
-        list[pd.DataFrmae]: list of langchain document 
+        list[pd.DataFrmae]: list of langchain document
     """
     if search_type == "hybrid":
         result = opensearch.hybrid_search(query)
@@ -37,6 +50,6 @@ def vectorstore_search(query:str, search_type:str="hybrid"):
     elif search_type == "cosine":
         result = opensearch.cosine_similarity_search(query)
 
-    result['text'].tolist()
+    result["text"].tolist()
 
     return result
