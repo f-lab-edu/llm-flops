6a657e02359bad0b4787125dd365df7f6401bb8d
seungbeom <tmdqja75@gmail.com>
2024-12-03 09:37:02 +0900

    [feature] langgraph tools (websearch, vectorstore search)  생성

---
+++ app/langgraph/tools.py
@@ -0,0 +1,42 @@
+from langchain_core.tools import tool
+from langchain_community.tools import DuckDuckGoSearchRun
+from vectorstore.opensearch_hybrid import OpenSearchHybridSearch
+
+# tool에 필요한 class 초기화
+duckduckgo_search = DuckDuckGoSearchRun()
+opensearch = OpenSearchHybridSearch()
+
+@tool(parse_docstring=True)
+def web_search(query: str):
+    """A tool to use when websearch is needed. Use this tool when there isn't enough information in OpenSearch vector store Database
+
+    Args:
+        query : a query to websearch
+
+    Returns:
+        list[str]: websearch result in list of strings
+    """
+    res = duckduckgo_search.invoke({"query": query})
+    return [res]
+
+@tool(parse_docstring=True)
+def vectorstore_search(query:str, search_type:str="hybrid"):
+    """search OpenSearch vector databsee for documents that are relevant to a given query
+
+    Args:
+        query: a query to search vector database
+        search_type (str, optional): one of three similarity search method: hybrid, BM25, and Cosine similarity search Defaults to "hybrid".
+
+    Returns:
+        list[pd.DataFrmae]: list of langchain document 
+    """
+    if search_type == "hybrid":
+        result = opensearch.hybrid_search(query)
+    elif search_type == "bm25":
+        result = opensearch.bm25_search(query)
+    elif search_type == "cosine":
+        result = opensearch.cosine_similarity_search(query)
+
+    result['text'].tolist()
+
+    return result
