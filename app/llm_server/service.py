800268e62326e00f84ae9c86be66bd3fd4496b4d
tmdqja75 <tmdqja75@gmail.com>
2024-12-11 18:00:07 +0900

    [fix] langgraph사용 위해 ChatHuggingFace모델 추가

--- app/llm_server/service.py
+++ app/llm_server/service.py
@@ -1,4 +1,6 @@
 import os
+import pprint
+
 import bentoml
 import torch
 import logging
@@ -27,28 +29,24 @@ class LlmService:
     def __init__(self):
         from transformers import AutoTokenizer, AutoModelForCausalLM
 
-        # API 서버에 가용 가능한 GPU를 설정
-        if torch.cuda.is_available():
-            self.device = torch.device("cuda")
-            bentoml_logger.info("Using CUDA for inference.")
-
-        elif torch.backends.mps.is_available():
-            self.device = torch.device("mps")
-            bentoml_logger.info("Using MPS (Apple Silicon) for inference.")
-        else:
-            # 사용 가능한 GPU가 없다면 cpu로 inference 대체
-            self.device = torch.device("cpu")
-            bentoml_logger.info("Using CPU for inference.")
-
-        # tokenizer/LLM 초기화
-        self.tokenizer = AutoTokenizer.from_pretrained(os.getenv("LLM_MODEL"))
-        self.model = AutoModelForCausalLM.from_pretrained(os.getenv("LLM_MODEL")).to(
-            self.device
+        self.device = get_device()
+        tokenizer = AutoTokenizer.from_pretrained(os.getenv("LLM_MODEL"))
+        model = AutoModelForCausalLM.from_pretrained(os.getenv("LLM_MODEL")).to(self.device)
+        model.generation_config.pad_token_id = tokenizer.eos_token_id
+        if tokenizer.chat_template is None:
+            raise NotFound("Tokenizer에 chat_template이 설정되지 않았습니다!")
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=int(os.getenv("MAX_TOKENS")),
+            device=self.device,
         )
-        # LLM모델이 open-end generation하지 않도록 padding token ID를 end-of-sentence token ID와 같게 설정
-        self.model.generation_config.pad_token_id = (
-            self.model.generation_config.eos_token_id
-        )
+
+        llm = HuggingFacePipeline(pipeline=pipe)
+        self.model = ChatHuggingFace(llm=llm)
+
+        self.app = build_graph(self.model)
 
         bentoml_logger.info(f"""{os.getenv("LLM_MODEL")} loaded to {self.device}""")
 
@@ -66,17 +64,28 @@ class LlmService:
         prompt = params.prompt
         temperature = params.temperature
 
-        inputs = self.tokenizer(prompt, return_tensors="pt")
-        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        # inputs = self.tokenizer(prompt, return_tensors="pt")
+        # inputs = {k: v.to(self.device) for k, v in inputs.items()}
 
-        # Generate text
-        with torch.no_grad():
-            outputs = self.model.generate(
-                **inputs,
-                temperature=temperature,
-                max_new_tokens=int(os.getenv("MAX_TOKENS")),
-            )
+        inputs = {
+            "query": prompt
+        }
+        for output in self.app.stream(inputs):
+            for key, value in output.items():
+                # Node
+                pprint(f"Node '{key}':")
+                # Optional: print full state at each node
+                # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+            pprint("\n---\n")
 
-        # Decode the generated text
-        generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return generated_text
+        # # Generate text
+        # with torch.no_grad():
+        #     outputs = self.model.generate(
+        #         **inputs,
+        #         temperature=temperature,
+        #         max_new_tokens=int(),
+        #     )
+
+        # # Decode the generated text
+        # generated_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        # return generated_text
