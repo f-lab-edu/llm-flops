84725d16856d480f50534186ef77e2448befc971
tmdqja75 <tmdqja75@gmail.com>
2025-01-05 00:48:45 +0900

    [fix] run_containers.sh에 env 변수 추가

--- scripts/run_containers.sh
+++ scripts/run_containers.sh
@@ -4,4 +4,4 @@
 docker run --rm -p 3001:3000 sentence_embedding_service:latest serve
 
 # LLM BentoML API 서버 실행
-docker run --rm -p 3000:3000 llm_service:latest serve
+docker run --rm -p 3000:3000 -e OPENAI_API_KEY=${OPENAI_API_KEY} -e OPENSEARCH_INITIAL_ADMIN_PASSWORD=${OPENSEARCH_INITIAL_ADMIN_PASSWORD} llm_service:latest serve
